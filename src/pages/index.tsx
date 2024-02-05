98e87cc29b4262c407dee659934723646a8a3afa
unknown <aiturgan888@gmail.com>
2024-02-04 23:31:15 -0600

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import { GlassNavigation } from "@/components/navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({subsets: ["latin"]});
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-9 ${inter.className}`}
+    >
+    <GlassNavigation/>
+      
+    </main>
+  );
+}
