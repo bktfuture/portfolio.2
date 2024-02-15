c4db4412b6c70f9e5d5be2a453e6751b3eec765b
unknown <aiturgan888@gmail.com>
2024-02-15 13:36:34 -0600

    cleaned the code and  made svg into components

---
+++ src/components/MainCubes/MediumCube.tsx
@@ -0,0 +1,50 @@
+export const MediumCube = () =>{
+    return(
+        <svg width="339" height="338" viewBox="0 0 344 338" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M1.88701 89.9493L175.482 171.5L346.336 84.3502L172.74 2.79956L1.88701 89.9493Z"
+                fill="#443D19"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+            <path
+                d="M1.88674 89.9494L175.482 171.5L178.224 340.2L4.62903 258.65L1.88674 89.9494Z"
+                fill="#897A32"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+            <path
+                d="M175.482 171.5L346.336 84.3502L349.078 253.051L178.225 340.2L175.482 171.5Z"
+                fill="#897A32"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+            <path
+                d="M172.74 2.79959L346.335 84.3502L349.078 253.051L175.482 171.5L172.74 2.79959Z"
+                fill="#B6A342"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+            <path
+                d="M172.74 2.79959L1.88667 89.9494L4.62895 258.65L175.482 171.5L172.74 2.79959Z"
+                fill="#B6A342"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+            <path
+                d="M4.62919 258.65L178.225 340.2L349.078 253.051L175.482 171.5L4.62919 258.65Z"
+                fill="#E4CC53"
+                stroke="#443D19"
+                stroke-width="2.31056"
+                stroke-miterlimit="1"
+            />
+    </svg>
+    )
+  }
+
+  export default MediumCube;
